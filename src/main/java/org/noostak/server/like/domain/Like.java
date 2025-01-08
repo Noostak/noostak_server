6da430941d33cd217f90425685361a6d3178d2f0
bbabbi <mooncheayoung@gmail.com>
2025-01-08 22:56:11 +0900

    feat(Like) : Like 엔티티 구현 - `id`: 좋아요 식별자

---
+++ src/main/java/org/noostak/server/like/domain/Like.java
@@ -0,0 +1,31 @@
+package org.noostak.server.like.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.noostak.server.appointment.domain.AppointmentMember;
+import org.noostak.server.global.common.BaseTimeEntity;
+import org.noostak.server.option.domain.Option;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appointment_members_id", nullable = false)
+    private AppointmentMember appointmentMember;
+
+    private Long likesCount;
+
+}
+
