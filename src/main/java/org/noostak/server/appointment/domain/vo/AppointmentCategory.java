22918410fabd0ad31e49819c426dbc4e364a35b5
권장순 <jsoonworld@gmail.com>
2025-01-10 20:13:06 +0900

    feat(AppointmentCategory): 약속 카테고리 Enum 구현
    - 약속의 카테고리를 관리하기 위해 `AppointmentCategory` Enum 클래스 생성
    - 카테고리로 `IMPORTANT(중요)`, `SCHEDULE(일정)`, `HOBBY(취미)`, `OTHER(기타)` 추가
    - 각 카테고리에 대응하는 메시지를 포함

---
+++ src/main/java/org/noostak/server/appointment/domain/vo/AppointmentCategory.java
@@ -0,0 +1,15 @@
+package org.noostak.server.appointment.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AppointmentCategory {
+    IMPORTANT("중요"),
+    SCHEDULE("일정"),
+    HOBBY("취미"),
+    OTHER("기타");
+
+    private final String message;
+}
