e381e111074f5cdd2402e6073f7d12f401be21b3
권장순 <jsoonworld@gmail.com>
2025-01-10 13:11:03 +0900

    refactor(Like) : 테스트 코드 실행을 위해 미구현된 부분 주석 처리

--- src/main/java/org/noostak/server/like/domain/Like.java
+++ src/main/java/org/noostak/server/like/domain/Like.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.noostak.server.appointment.domain.AppointmentMember;
 import org.noostak.server.global.entity.BaseTimeEntity;
-import org.noostak.server.option.domain.Option;
 
 @Entity
 @Getter
@@ -17,9 +16,9 @@ public class Like extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long likeId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "option_id", nullable = false)
-    private Option option;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "option_id", nullable = false)
+//    private Option option;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "appointment_members_id", nullable = false)
