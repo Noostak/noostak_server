269b40a26b948d3cc4823f32f4c1eb0183e54c87
권장순 <jsoonworld@gmail.com>
2025-01-12 01:07:19 +0900

    chore(group): 불필요한 temp 파일 및 import 정리
    - 패키지 구조 정리를 위해 생성했던 `temp` 파일 삭제
    - 사용되지 않는 import 제거

--- src/main/java/org/noostak/server/group/application/GroupService.java
+++ src/main/java/org/noostak/server/group/application/GroupService.java
@@ -1,11 +1,7 @@
 package org.noostak.server.group.application;
 
 import lombok.RequiredArgsConstructor;
-import org.noostak.server.group.domain.Group;
 import org.noostak.server.group.domain.GroupRepository;
-import org.noostak.server.group.domain.vo.GroupImageUrl;
-import org.noostak.server.group.domain.vo.GroupMemberCount;
-import org.noostak.server.group.domain.vo.GroupName;
 import org.noostak.server.group.dto.response.GroupResponse;
 import org.noostak.server.group.dto.response.GroupsResponse;
 import org.springframework.stereotype.Service;
