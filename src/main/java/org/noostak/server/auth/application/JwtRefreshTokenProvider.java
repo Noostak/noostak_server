ba9c9077eef2c4de991623ace72a56f371b21637
bbabbi <mooncheayoung@gmail.com>
2025-01-12 04:15:33 +0900

    feat(JwtRefreshTokenProvider) : JwtRefreshTokenProvider 추가 - 리프레시 토큰 발급 및 검증 구현 - issueToken 메서드 : 리프레시 토큰 발급을 위한 로직을 구현.      - Clock을 사용하여 현재 시간을 가져오고, 만료 시간(expiration)을 계산      - UUID를 사용해 토큰의 고유 ID(jti)를 생성.      - JwtTokenProvider의 서명 키와 HMAC-SHA256 알고리즘으로 서명하여 토큰을 생성. - getClaimsFromToken 메서드: 전달받은 리프레시 토큰을 파싱하여 클레임(Claims)을 추출.      - 서명 키를 사용하여 토큰의 무결성을 검증.

---
+++ src/main/java/org/noostak/server/auth/application/JwtRefreshTokenProvider.java
@@ -0,0 +1,49 @@
+package org.noostak.server.auth.application;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRefreshTokenProvider {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final Clock clock;
+
+    @Value("${jwt.expiration.refresh-token}")
+    private Long refreshTokenExpirationTime;
+
+    public String issueToken() {
+        final Instant nowInstant = clock.instant();
+        final Date now = Date.from(nowInstant); // Instant → Date 변환
+        final Date expiration = Date.from(nowInstant.plusMillis(refreshTokenExpirationTime));
+
+        final Claims claims = Jwts.claims()
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(now)
+                .setExpiration(expiration);
+
+        return Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setClaims(claims)
+                .signWith(jwtTokenProvider.getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtTokenProvider.getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
