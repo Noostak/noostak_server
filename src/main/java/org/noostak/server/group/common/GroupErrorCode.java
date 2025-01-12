031897e506310390f956b794fccc7dbd3a88ed0b
권장순 <jsoonworld@gmail.com>
2025-01-12 21:16:48 +0900

    feat(GroupErrorCode) : 그룹 관련 에러 코드 추가 및 수정
    - 새로운 에러 코드 추가
      - INVALID_GROUP_NAME_LENGTH: 그룹 이름 길이 제한 초과 시 에러
      - GROUP_NAME_NOT_EMPTY: 그룹 이름이 비어 있는 경우 에러
      - INVALID_GROUP_IMAGE_URL: 유효하지 않은 그룹 이미지 URL 에러
      - INVALID_INVITE_CODE: 초대 코드가 정책에 부합하지 않는 경우 에러
      - LEADER_NOT_FOUND: 그룹 리더를 찾을 수 없는 경우 에러
      - LEADER_NOT_SET: 그룹 리더가 설정되지 않은 경우 에러
    - 기존 코드 중복 제거
      - GROUP_NOT_FOUND 중복 선언 제거
    - PREFIX 상수 추가
      - 에러 메시지 식별을 위한 "[GROUP ERROR] " 접두사 추가

--- src/main/java/org/noostak/server/group/common/GroupErrorCode.java
+++ src/main/java/org/noostak/server/group/common/GroupErrorCode.java
@@ -8,9 +8,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum GroupErrorCode implements ErrorCode {
-    INVALID_GROUP_NAME(HttpStatus.BAD_REQUEST, "유효하지 않은 그룹 이름입니다."),
-    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "그룹을 찾을 수 없습니다.");
+    // 그룹 조회
+    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "그룹을 찾을 수 없습니다."),
 
+    // 그룹 생성
+    INVALID_GROUP_NAME(HttpStatus.BAD_REQUEST, "유효하지 않은 그룹 이름입니다."),
+    INVALID_GROUP_NAME_LENGTH(HttpStatus.BAD_REQUEST, "그룹 이름의 길이는 30글자를 넘을 수 없습니다."),
+    GROUP_NAME_NOT_EMPTY(HttpStatus.BAD_REQUEST, "그룹 이름은 비어 있을 수 없습니다."),
+    INVALID_GROUP_IMAGE_URL(HttpStatus.BAD_REQUEST, "그룹 이미지 URL은 유효한 URL이어야 합니다."),
+    INVALID_INVITE_CODE(HttpStatus.INTERNAL_SERVER_ERROR, "생성된 초대 코드가 정책에 부합하지 않습니다."),
+    LEADER_NOT_FOUND(HttpStatus.NOT_FOUND, "리더를 찾을 수 없습니다."),
+    LEADER_NOT_SET(HttpStatus.INTERNAL_SERVER_ERROR, "리더가 설정되지 않았습니다."),
+
+
+    ;
     public static final String PREFIX = "[GROUP ERROR] ";
 
     private final HttpStatus status;
