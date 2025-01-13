1eda3b6619541cefffc0f96e2e66f0a93d2954d3
bbabbi <mooncheayoung@gmail.com>
2025-01-13 09:26:26 +0900

    chore(ClockConfig) : 주석 제거 - 클린 코드 작성을 위한 불필요한 주석 제거

--- src/main/java/org/noostak/server/global/config/ClockConfig.java
+++ src/main/java/org/noostak/server/global/config/ClockConfig.java
@@ -8,9 +8,6 @@ import java.time.Clock;
 @Configuration
 public class ClockConfig {
 
-    /**
-     * 시스템 기본 시간대를 사용하는 Clock 빈 생성
-     */
     @Bean
     public Clock clock() {
         return Clock.systemDefaultZone();
