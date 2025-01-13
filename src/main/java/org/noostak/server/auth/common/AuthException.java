fc049e6d36e03a28202e5da9eb5afd331a8d9f2a
bbabbi <mooncheayoung@gmail.com>
2025-01-14 06:11:02 +0900

    refactor(AuthException) : Throwable 설정 추가 - Throwable을 처리할 수 있는 생성자를 추가 - initCause 메서드는 Throwable의 기본 메서드로, 이미 설정된 Throwable의 원인을 추가로 설정 가능 - 공통으로 사용하는 BaseException을 건드리기 보다는 AuthException에서 수정해서 사용하는 게 맞다고 생각해서 AuthException에 Throwable 설정을 추가

--- src/main/java/org/noostak/server/auth/common/AuthException.java
+++ src/main/java/org/noostak/server/auth/common/AuthException.java
@@ -3,7 +3,13 @@ package org.noostak.server.auth.common;
 import org.noostak.server.global.error.core.BaseException;
 
 public class AuthException extends BaseException {
+
     public AuthException(AuthErrorCode errorCode) {
         super(errorCode);
+    }
+
+    public AuthException(AuthErrorCode errorCode, Throwable cause) {
+        super(errorCode);
+        initCause(cause);
     }
 }
