782d3db39bcaee349aec071bf8a0475af8931980
bbabbi <mooncheayoung@gmail.com>
2025-01-13 10:03:23 +0900

    refactor(AuthSuccessCode) : 상태 코드 타입 변경 및 HttpStatus 적용 - AuthSuccessCode enum의 상태 코드를 int에서 HttpStatus로 변경 - HttpStatus를 사용하여 가독성과 상태 코드의 의미 명확성 향상

--- src/main/java/org/noostak/server/auth/common/AuthSuccessCode.java
+++ src/main/java/org/noostak/server/auth/common/AuthSuccessCode.java
@@ -3,15 +3,16 @@ package org.noostak.server.auth.common;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.noostak.server.global.success.handler.SuccessCode;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum AuthSuccessCode implements SuccessCode {
-    VALID_JWT(200, "JWT 토큰이 유효합니다."),
-    REFRESHED_JWT(200, "JWT 토큰이 성공적으로 갱신되었습니다."),
-    LOGGED_IN(200, "로그인에 성공했습니다."),
-    LOGGED_OUT(200, "로그아웃에 성공했습니다.");
+    VALID_JWT(HttpStatus.OK, "JWT 토큰이 유효합니다."),
+    REFRESHED_JWT(HttpStatus.OK, "JWT 토큰이 성공적으로 갱신되었습니다."),
+    LOGGED_IN(HttpStatus.OK, "로그인에 성공했습니다."),
+    LOGGED_OUT(HttpStatus.OK, "로그아웃에 성공했습니다.");
 
-    private final int status;
+    private final HttpStatus status;
     private final String message;
 }
