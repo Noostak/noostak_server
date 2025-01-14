3783b1ca0fe61386214c50da72b4fcf0fce5e791
bbabbi <mooncheayoung@gmail.com>
2025-01-14 17:46:27 +0900

    test(S3ServiceTest) : 이미지 업로드 성공 테스트 추가 - 올바른 확장자와 크기를 가진 이미지가 정상적으로 업로드되는지 검증하는 테스트 추가 - S3Client의 putObject 메서드 호출 여부를 확인

--- src/test/java/org/noostak/server/infra/S3ServiceTest.java
+++ src/test/java/org/noostak/server/infra/S3ServiceTest.java
@@ -1,9 +1,19 @@
 package org.noostak.server.infra;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.noostak.server.global.config.AwsConfig;
 
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 class S3ServiceTest {
@@ -12,4 +22,32 @@ class S3ServiceTest {
     private final S3Client s3Client = mock(S3Client.class);
     private final String bucketName = "test-bucket";
     private final S3Service s3Service = new S3Service(bucketName, awsConfig);
+
+    @Nested
+    @DisplayName("Image Upload Tests")
+    class UploadImageTests {
+
+        @Test
+        @DisplayName("이미지 업로드 - 성공")
+        void uploadImage_success() throws IOException {
+
+            // Given
+            String directoryPath = "images/";
+            MultipartFile image = new MockMultipartFile(
+                    "image",
+                    "test.jpg",
+                    "image/jpeg",
+                    new byte[]{1, 2, 3, 4}
+            );
+            when(awsConfig.getS3Client()).thenReturn(s3Client);
+
+            // When
+            String result = s3Service.uploadImage(directoryPath, image);
+
+            // Then
+            assertThat(result).startsWith(directoryPath).endsWith(".jpg");
+            verify(s3Client).putObject(any(PutObjectRequest.class), any(RequestBody.class));
+        }
+    }
+
 }
