ec605174585bf198f545924f25021b0a03c20719
권장순 <jsoonworld@gmail.com>
2025-01-14 12:54:54 +0900

    test(AppointmentRepositoryTest) : 약속 리포지토리 테스트용 인터페이스 구현

---
+++ src/test/java/org/noostak/server/appointment/domain/AppointmentRepositoryTest.java
@@ -0,0 +1,6 @@
+package org.noostak.server.appointment.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepositoryTest extends JpaRepository<Appointment, Long> {
+}
