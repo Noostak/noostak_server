40e1bab1cd6c4c8bef1706a68d21ee0874297524
bbabbi <mooncheayoung@gmail.com>
2025-01-18 00:50:20 +0900

    refactor(AwsCongfig) : AwsProperty 주입 방식 변경 - AwsConfig 클래스에서 AwsProperties 객체 주입 방식을 @Value 애노테이션으로 변경 - S3 버킷 이름, 액세스 키, 시크릿 키, AWS 리전 등의 프로퍼티를 @Value로 명시적으로 주입 - 코드 간결성과 명확성을 위해 AwsProperties 클래스 사용 제거 - AWS Region 및 S3Client Bean 생성 로직은 기존과 동일하게 유지 - 자격증명을 코드에서 분리하기 위해 DefaultCredentialsProvider 사용

--- src/main/java/org/noostak/server/global/config/AwsConfig.java
+++ src/main/java/org/noostak/server/global/config/AwsConfig.java
@@ -1,5 +1,7 @@
 package org.noostak.server.global.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
@@ -7,17 +9,28 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Getter
 public class AwsConfig {
 
-    private final AwsProperties awsProperties;
+    @Value("${aws-property.s3-bucket-name}")
+    private String s3BucketName;
 
-    public AwsConfig(AwsProperties awsProperties) {
-        this.awsProperties = awsProperties;
-    }
+    @Value("${aws-property.access-key}")
+    private String accessKey;
+
+    @Value("${aws-property.secret-key}")
+    private String secretKey;
+
+    @Value("${aws-property.aws-region}")
+    private String awsRegion;
+
+    @Value("${aws-property.max-file-size}")
+    private String maxfileSize;
 
     @Bean
     public Region getRegion() {
-        return Region.of(awsProperties.getAwsRegion());
+
+        return Region.of(awsRegion);
     }
 
     @Bean
