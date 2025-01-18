0ba2d49f7a349d6014b631be7fb0fa6c93f858cc
권장순 <jsoonworld@gmail.com>
2025-01-18 19:55:59 +0900

    test(GroupController) : GetAllGroupsControllerTest 리팩터링 및 테스트 데이터 초기화 개선
    - 테스트 데이터 초기화 개선
      - `setUp` 메서드에서 그룹 초기화 데이터를 정리하고 중복 코드를 제거했습니다.
      - 그룹 리더 ID를 명확히 구분하고, 각 그룹의 데이터를 직관적으로 관리하도록 수정했습니다.
    
    - 그룹 생성 데이터 수정
      - `Group.of` 메서드를 활용하여 테스트 그룹 데이터를 생성하며, URL과 초대 코드를 명확히 설정했습니다.
      - 그룹 리더와 호스트 ID를 일관되게 관리하여 테스트 데이터의 신뢰성을 높였습니다.

--- src/test/java/org/noostak/server/group/api/GetAllGroupsControllerTest.java
+++ src/test/java/org/noostak/server/group/api/GetAllGroupsControllerTest.java
@@ -3,14 +3,19 @@ package org.noostak.server.group.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.noostak.server.group.domain.Group;
 import org.noostak.server.group.domain.GroupRepository;
+import org.noostak.server.group.domain.GroupRepositoryTest;
 import org.noostak.server.group.domain.vo.GroupImageUrl;
 import org.noostak.server.group.domain.vo.GroupName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,22 +33,21 @@ class GetAllGroupsControllerTest {
 
     @BeforeEach
     void setUp() {
-        Long firstLeaderId = 1L;
-        Long secondLeaderId = 2L;
 
-        groupRepository.deleteAll();
         groupRepository.save(Group.of(
-                firstLeaderId,
+                1L,
                 GroupName.from("Group 1"),
                 GroupImageUrl.from("https://example.com/group1.png"),
                 "123ABC"
         ));
+
         groupRepository.save(Group.of(
-                secondLeaderId,
+                2L,
                 GroupName.from("Group 2"),
                 GroupImageUrl.from("https://example.com/group2.png"),
                 "ABC456"
         ));
+
     }
 
     @Test
@@ -57,4 +61,5 @@ class GetAllGroupsControllerTest {
                 .andExpect(jsonPath("$.result.groups[0].groupName").value("Group 1"))
                 .andExpect(jsonPath("$.result.groups[1].groupName").value("Group 2"));
     }
+
 }
