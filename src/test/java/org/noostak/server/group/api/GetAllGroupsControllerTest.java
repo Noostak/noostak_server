b6172a017ab8dc38f525ffc06b1fa0516036aaf1
권장순 <jsoonworld@gmail.com>
2025-01-18 20:07:44 +0900

    refactor(GroupController) : 테스트 클래스 초기화 리팩터링 및 불필요한 코드 제거
    - 불필요한 import 제거
      - 사용되지 않는 `Mockito`, `MockitoBean` 관련 import 문을 삭제하여 코드를 정리했습니다.
    
    - 테스트 클래스 초기화 리팩터링
      - 테스트에 필요한 import만 유지하여 가독성과 유지보수성을 개선했습니다.
      - `java.util.List`와 같은 미사용 코드 제거.

--- src/test/java/org/noostak/server/group/api/GetAllGroupsControllerTest.java
+++ src/test/java/org/noostak/server/group/api/GetAllGroupsControllerTest.java
@@ -3,19 +3,14 @@ package org.noostak.server.group.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.noostak.server.group.domain.Group;
 import org.noostak.server.group.domain.GroupRepository;
-import org.noostak.server.group.domain.GroupRepositoryTest;
 import org.noostak.server.group.domain.vo.GroupImageUrl;
 import org.noostak.server.group.domain.vo.GroupName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
