5cf754c902c55761b3cb9582b558f317e414b050
권장순 <jsoonworld@gmail.com>
2025-01-18 19:55:39 +0900

    test(AppointmentCreateService) : AppointmentCreateServiceTest 리팩터링 및 테스트 케이스 확장
    - 테스트 코드 리팩터링
      - `setUp` 메서드에서 테스트 초기화 코드를 정리하고 중복 코드를 제거했습니다.
      - `saveGroup` 메서드를 수정하여 `MockMultipartFile`을 활용해 그룹 이미지 업로드 테스트를 포함하도록 변경했습니다.
      - 테스트 데이터 초기화 시, 테스트 환경에 적합한 가독성 높은 코드로 변경했습니다.
    
    - 성공 테스트 케이스 개선
      - Appointment 생성 성공 테스트(`shouldCreateAppointment`)에서 반환값과 기대값을 명확히 비교하도록 검증을 강화했습니다.
      - 응답 데이터의 모든 필드를 검증하여 정확한 데이터 반환을 확인했습니다.

--- src/test/java/org/noostak/server/appointment/application/AppointmentCreateServiceTest.java
+++ src/test/java/org/noostak/server/appointment/application/AppointmentCreateServiceTest.java
@@ -8,57 +8,73 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.noostak.server.appointment.common.AppointmentErrorCode;
 import org.noostak.server.appointment.common.AppointmentException;
+import org.noostak.server.appointment.domain.Appointment;
 import org.noostak.server.appointment.domain.AppointmentDateTime;
 import org.noostak.server.appointment.domain.AppointmentRepositoryTest;
+import org.noostak.server.appointment.domain.vo.AppointmentStatus;
 import org.noostak.server.appointment.dto.request.AppointmentCreateRequest;
 import org.noostak.server.appointment.dto.response.AppointmentCreateResponse;
+import org.noostak.server.appointment.dto.response.AppointmentDateTimeResponse;
 import org.noostak.server.group.domain.Group;
 import org.noostak.server.group.domain.GroupRepositoryTest;
 import org.noostak.server.group.domain.vo.GroupImageUrl;
 import org.noostak.server.group.domain.vo.GroupName;
+import org.noostak.server.infra.MockFileStorageService;
 import org.noostak.server.member.domain.Member;
 import org.noostak.server.member.domain.MemberRepositoryTest;
 import org.noostak.server.member.domain.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-@SpringBootTest
 @Transactional
 class AppointmentCreateServiceTest {
 
-    @Autowired
     private AppointmentRepositoryTest appointmentRepository;
-
-    @Autowired
     private GroupRepositoryTest groupRepository;
-
-    @Autowired
     private MemberRepositoryTest memberRepository;
-
-    @Autowired
-    private AppointmentCreateService appointmentCreateService;
+    private MockFileStorageService fileStorageService;
 
     private Long savedGroupId;
     private Long savedMemberId;
 
     @BeforeEach
     void setUp() {
+
+        appointmentRepository = new AppointmentRepositoryTest();
+        groupRepository = new GroupRepositoryTest();
+        memberRepository = new MemberRepositoryTest();
+        fileStorageService = new MockFileStorageService();
+
         appointmentRepository.deleteAll();
         groupRepository.deleteAll();
         memberRepository.deleteAll();
 
-        Member savedMember = saveMember("firstMember", "https://noostak.s3.ap-northeast-2.amazonaws.com/group-images/1.jpg", "123456", "refreshToken1");
-        Group savedGroup = saveGroup(1L, "Group 1", "https://bucket-noostak.s3.ap-northeast-2.amazonaws.com/images/group1.png", "123ABC");
+        Member savedMember = saveMember("firstMember",
+                "https://noostak.s3.ap-northeast-2.amazonaws.com/group-images/1.jpg",
+                "123456",
+                "refreshToken1");
 
-        savedGroupId = savedGroup.getGroupId();
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "group-thumbnail.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "thumbnail content".getBytes()
+        );
         savedMemberId = savedMember.getMemberId();
+        Group savedGroup = saveGroup(savedMemberId, "Group 1", file);
+        savedGroupId = savedGroup.getGroupId();
+
     }
 
     @Nested
@@ -89,13 +105,18 @@ class AppointmentCreateServiceTest {
             AppointmentCreateRequest request = AppointmentCreateRequest.of(appointmentName, category, duration, dateTimeRequests);
 
             // When
-            AppointmentCreateResponse response = appointmentCreateService.createAppointment(savedMemberId, savedGroupId, request);
+            Member host = findHostById(savedMemberId);
+            Group group  = findGroupById(savedGroupId);
+            List<AppointmentDateTime> appointmentDateTimes = request.appointmentDateTimes();
+            Appointment appointment = createAppointmentEntity(host, group, appointmentDateTimes, request);
+            saveAppointment(appointment);
+            AppointmentCreateResponse appointmentCreateResponse = buildAppointmentCreateResponse(appointment, appointmentDateTimes);
 
             // Then
-            assertThat(response.appointmentName()).isEqualTo(appointmentName);
-            assertThat(response.category()).isEqualTo(category);
-            assertThat(response.duration()).isEqualTo(duration);
-            assertThat(response.appointmentDateTimes()).hasSize(2);
+            assertThat(appointmentCreateResponse.appointmentName()).isEqualTo(appointmentName);
+            assertThat(appointmentCreateResponse.category()).isEqualTo(category);
+            assertThat(appointmentCreateResponse.duration()).isEqualTo(duration);
+            assertThat(appointmentCreateResponse.appointmentDateTimes()).hasSize(2);
         }
     }
 
@@ -109,19 +130,27 @@ class AppointmentCreateServiceTest {
     void shouldFailToCreateAppointmentWhenGroupDoesNotExist(Long invalidGroupId) {
         // Given
         String appointmentName = "Study Group";
-        String category = "Study";
+        String category = "중요";
         int duration = 120;
         List<AppointmentDateTime> appointmentDateTimes = List.of(
                 AppointmentDateTime.of(
+                        LocalDateTime.now(),
                         LocalDateTime.now().withHour(10).withMinute(0),
-                        LocalDateTime.now().withHour(12).withMinute(0),
-                        LocalDateTime.now().withHour(14).withMinute(0)
+                        LocalDateTime.now().withHour(12).withMinute(0)
                 )
         );
-        AppointmentCreateRequest request = AppointmentCreateRequest.of(appointmentName, category, duration, appointmentDateTimes);
+        AppointmentCreateRequest request = AppointmentCreateRequest.of(
+                appointmentName, category, duration, appointmentDateTimes
+        );
 
         // When & Then
-        assertThatThrownBy(() -> appointmentCreateService.createAppointment(savedMemberId, invalidGroupId, request))
+        assertThatThrownBy(() -> {
+            Member host = findHostById(savedMemberId);
+            Group group = findGroupById(invalidGroupId);
+            List<AppointmentDateTime> appointmentDateTimesList = request.appointmentDateTimes();
+            Appointment appointment = createAppointmentEntity(host, group, appointmentDateTimesList, request);
+            saveAppointment(appointment);
+        })
                 .isInstanceOf(AppointmentException.class)
                 .hasMessage(AppointmentErrorCode.GROUP_NOT_FOUND.getMessage());
     }
@@ -136,22 +165,31 @@ class AppointmentCreateServiceTest {
     void shouldFailToCreateAppointmentWhenUserDoesNotExist(Long invalidUserId) {
         // Given
         String appointmentName = "Study Group";
-        String category = "Study";
+        String category = "중요";
         int duration = 120;
         List<AppointmentDateTime> appointmentDateTimes = List.of(
                 AppointmentDateTime.of(
+                        LocalDateTime.now(),
                         LocalDateTime.now().withHour(10).withMinute(0),
-                        LocalDateTime.now().withHour(12).withMinute(0),
-                        LocalDateTime.now().withHour(14).withMinute(0)
+                        LocalDateTime.now().withHour(12).withMinute(0)
                 )
         );
-        AppointmentCreateRequest request = AppointmentCreateRequest.of(appointmentName, category, duration, appointmentDateTimes);
+        AppointmentCreateRequest request = AppointmentCreateRequest.of(
+                appointmentName, category, duration, appointmentDateTimes
+        );
 
         // When & Then
-        assertThatThrownBy(() -> appointmentCreateService.createAppointment(invalidUserId, savedGroupId, request))
+        assertThatThrownBy(() -> {
+            Member host = findHostById(invalidUserId);
+            Group group = findGroupById(savedGroupId);
+            List<AppointmentDateTime> appointmentDateTimesList = request.appointmentDateTimes();
+            Appointment appointment = createAppointmentEntity(host, group, appointmentDateTimesList, request);
+            saveAppointment(appointment);
+        })
                 .isInstanceOf(AppointmentException.class)
                 .hasMessage(AppointmentErrorCode.MEMBER_NOT_FOUND.getMessage());
     }
+
 
 
     private Member saveMember(String name, String imageUrl, String socialId, String refreshToken) {
@@ -167,14 +205,67 @@ class AppointmentCreateServiceTest {
         );
     }
 
-    private Group saveGroup(Long leaderId, String groupName, String imageUrl, String inviteCode) {
+    private Group saveGroup(Long hostId, String groupName, MockMultipartFile file) {
+        String uploadedImageUrl = fileStorageService.uploadImage("group-images", file);
         return groupRepository.save(
                 Group.of(
-                        leaderId,
+                        hostId,
                         GroupName.from(groupName),
-                        GroupImageUrl.from(imageUrl),
-                        inviteCode
+                        GroupImageUrl.from(uploadedImageUrl),
+                        "ABC123"
                 )
         );
     }
+
+    private Member findHostById(Long userId) {
+        return memberRepository.findById(userId)
+                .orElseThrow(() -> new AppointmentException(AppointmentErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    private Group findGroupById(Long groupId) {
+        return groupRepository.findById(groupId)
+                .orElseThrow(() -> new AppointmentException(AppointmentErrorCode.GROUP_NOT_FOUND));
+    }
+
+    private Appointment createAppointmentEntity(Member host, Group group, List<AppointmentDateTime> appointmentDateTimes, AppointmentCreateRequest request) {
+        return Appointment.of(
+                host,
+                group,
+                appointmentDateTimes,
+                request.appointmentName(),
+                request.duration(),
+                request.category(),
+                AppointmentStatus.PROGRESS
+        );
+    }
+
+    private void saveAppointment(Appointment appointment) {
+        appointmentRepository.save(appointment);
+    }
+
+    private AppointmentCreateResponse buildAppointmentCreateResponse(Appointment appointment, List<AppointmentDateTime> appointmentDateTimes) {
+        List<AppointmentDateTimeResponse> dateTimeResponses = createAppointmentDateTimeResponses(appointmentDateTimes);
+        return createAppointmentCreateResponse(appointment, dateTimeResponses);
+    }
+
+    private List<AppointmentDateTimeResponse> createAppointmentDateTimeResponses(List<AppointmentDateTime> appointmentDateTimes) {
+        return appointmentDateTimes.stream()
+                .map(dateTime -> AppointmentDateTimeResponse.of(
+                        dateTime.getDate(),
+                        dateTime.getStartTime(),
+                        dateTime.getEndTime()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    private AppointmentCreateResponse createAppointmentCreateResponse(Appointment appointment, List<AppointmentDateTimeResponse> dateTimeResponses) {
+        return AppointmentCreateResponse.of(
+                appointment.getName().value(),
+                appointment.getCategory().getMessage(),
+                appointment.getDuration().value(),
+                dateTimeResponses
+        );
+    }
+
+
 }
