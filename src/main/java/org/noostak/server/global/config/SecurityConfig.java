d93a119501be440b4d6a5743759a594a674f838d
bbabbi <mooncheayoung@gmail.com>
2025-01-19 06:17:26 +0900

    feat(SecurityConfig) : JwtAuthenticationFilter 추가 및 SecurityFilterChain에 적용 - JwtAuthenticationFilter를 Spring Security의 필터 체인에 추가 -> JWT 기반 인증 로직을 적용 - 필터는 UsernamePasswordAuthenticationFilter 이전에 실행

--- src/main/java/org/noostak/server/global/config/SecurityConfig.java
+++ src/main/java/org/noostak/server/global/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package org.noostak.server.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.noostak.server.auth.application.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     private static final String[] WHITE_LIST = {
             "/api/auth/sign-in",
@@ -26,7 +32,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(WHITE_LIST).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
