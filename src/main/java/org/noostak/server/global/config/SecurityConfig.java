51b4a98a298ed47c26c4bd41a9365ca9ef9975bf
bbabbi <mooncheayoung@gmail.com>
2025-01-19 06:15:59 +0900

    feat(SecurityConfig) : 요청별 인증 및 허용 정책 추가 - 특정 요청 경로(화이트리스트)에 대해 인증 없이 접근을 허용 - 그 외 요청은 인증이 필요하도록 설정

--- src/main/java/org/noostak/server/global/config/SecurityConfig.java
+++ src/main/java/org/noostak/server/global/config/SecurityConfig.java
@@ -9,12 +9,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private static final String[] WHITE_LIST = {
+            "/api/auth/sign-in",
+            "/api/auth/sign-up",
+            "/api/auth/token-reissue",
+            "/actuator/health"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
